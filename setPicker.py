27e27bd1e83347548ef49b2492065130f6f4fdc0
Andrew Cerni <andrewcerni@Josh-Ms-MacBook-Air.local>
2015-10-05 22:00:57 -0400

    formatting and code small code changes/fixes

--- setPicker.py
+++ setPicker.py
@@ -5,119 +5,119 @@ class setPicker:
 	def __init__(self):
 		self.pickNum = 1
 		self.picked = []
-		self.coreSets = [
-					'Alpha',
-					'Beta',
-					'Revised',
-					'Ultd',
-					'IV',
-					'V',
-					'VI',
-					'7th',
-					'8th',
-					'9th',
-					'Xth',
-					'M10',
-					'M11',
-					'M12',
-					'M13',
-					'M14',
-					'M15',
-					'Origins'
-				]
+		self.coreSets	=	[
+							'Alpha',
+							'Beta',
+							'Revised',
+							'Ultd',
+							'IV',
+							'V',
+							'VI',
+							'7th',
+							'8th',
+							'9th',
+							'Xth',
+							'M10',
+							'M11',
+							'M12',
+							'M13',
+							'M14',
+							'M15',
+							'Origins'
+							]
 
-		self.smallSets = 	[	'Arabian Nights',
-						'Antiques',
-						'The Dark',
-						'Fallen Empires',
-						'Homelands',
-						'Alliances',
-						'Coldsnap',
-						'Visions',
-						'Weatherlight',
-						'Stronghold',
-						'Exodus',
-						'Urzas Legacy',
-						'Urzas destiny',
-						'Nemesis',
-						'Prophecy',
-						'Planeshift',
-						'Apocalypse',
-						'Torment',
-						'Judgement',
-						'Legions',
-						'Scourge',
-						'Darksteel',
-						'5thDawn',
-						'Betrayers',
-						'Saviors',
-						'Guildpact',
-						'Dissention',
-						'Planar Chaos',
-						'Future Sight',
-						'Morningtide',
-						'Eventide',
-						'Conflux',
-						'Alara Reborn',
-						'Worldwake',
-						'Eldrazi',
-						'Beseiged',
-						'Phyrexia',
-						'Dark Ascension',
-						'Gatecrash',
-						'Dragon Maze',
-						'Born of the Gods',
-						'Journey into Nyx',
-						'Fate Reforged'
-					]
+		self.smallSets = 	[	
+							'Arabian Nights',
+							'Antiques',
+							'The Dark',
+							'Fallen Empires',
+							'Homelands',
+							'Alliances',
+							'Coldsnap',
+							'Visions',
+							'Weatherlight',
+							'Stronghold',
+							'Exodus',
+							'Urzas Legacy',
+							'Urzas destiny',
+							'Nemesis',
+							'Prophecy',
+							'Planeshift',
+							'Apocalypse',
+							'Torment',
+							'Judgement',
+							'Legions',
+							'Scourge',
+							'Darksteel',
+							'5thDawn',
+							'Betrayers',
+							'Saviors',
+							'Guildpact',
+							'Dissention',
+							'Planar Chaos',
+							'Future Sight',
+							'Morningtide',
+							'Eventide',
+							'Conflux',
+							'Alara Reborn',
+							'Worldwake',
+							'Beseiged',
+							'Phyrexia',
+							'Dark Ascension',
+							'Dragon Maze',
+							'Born of the Gods',
+							'Journey into Nyx',
+							'Fate Reforged'
+							]
 
 		self.largeSets = 	[
-						'Legends',
-						'Ice Age',
-						'Mirage',
-						'Tempest',
-						'Urzas saga',
-						'Masques',
-						'Invasion',
-						'Oddysey',
-						'Onslaught',
-						'Mirrodin',
-						'Champions of Kamigawa',
-						'Ravnica: city of guilds',
-						'Time Spiral',
-						'Lorwyn',
-						'Shadowmoor',
-						'Shards',
-						'Zendikar',
-						'Scars',
-						'Innistrad',
-						'Return to Ravnica',
-						'Theros',
-						'Khans',
-						'Dragons'
-					]
-
-		self.pick1 = [ 'Onslaught', 'Shards', 'M12', 'M13', 'Worldwake', 'Antiques']
+							'Legends',
+							'Ice Age',
+							'Mirage',
+							'Tempest',
+							'Urzas saga',
+							'Masques',
+							'Invasion',
+							'Oddysey',
+							'Onslaught',
+							'Mirrodin',
+							'Champions of Kamigawa',
+							'Ravnica: city of guilds',
+							'Time Spiral',
+							'Lorwyn',
+							'Shadowmoor',
+							'Shards',
+							'Zendikar',
+							'Eldrazi',
+							'Scars',
+							'Innistrad',
+							'Avacyn Restored',
+							'Return to Ravnica',
+							'Gatecrash',
+							'Theros',
+							'Khans',
+							'Dragons'
+							]
 
 	def __pickCore(self):
 		pick = random.choice(range(len(self.coreSets)))
-		set = self.coreSets[pick]
+		theSet = self.coreSets[pick]
 		del self.coreSets[pick]
-		return set
+		return theSet
 
 	def __pickSmall(self):
 		pick = random.choice(range(len(self.smallSets)))
-		set = self.smallSets[pick]
+		theSet = self.smallSets[pick]
 		del self.smallSets[pick]
-		return set
+		return theSet
 
 	def __pickLarge(self):
 		pick = random.choice(range(len(self.largeSets)))
-		set = self.largeSets[pick]
+		theSet = self.largeSets[pick]
 		del self.largeSets[pick]
-		return set
+		return theSet
 
-	def __save(self, picked):
+	def __save(self):
 		try:
 			os.mkdir('./pickles')
 		except OSError:
@@ -125,16 +125,15 @@ class setPicker:
 		
 		attrName = 'picked_'+str(self.pickNum)
 		self.pickNum += 1
-		setattr(self, attrName, picked)
+		setattr(self, attrName, self.picked)
 
 		timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 		file = open('./pickles/'+timestamp+'.sav', 'w')
 		pickle.dump(self, file)
-		self
 		file.close()
 
 	def pick(self):
 		self.picked = [self.__pickLarge(), self.__pickLarge(), self.__pickSmall(),  self.__pickSmall(),  self.__pickSmall(),  self.__pickSmall(), self.__pickCore(), self.__pickCore()]
 		print self.picked		
-		self.__save(self.picked)
+		self.__save()
 
